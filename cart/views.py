5412003b4af323b0b1d5161e21d9fa4b7075c389
Sergey N <snorov@gmail.com>
2020-01-16 16:22:12 +0300

    recommendation products
    add recommendations

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 from coupons.forms import CouponApplyForm
+from shop.recommender import Recommender
 
 
 @require_POST
@@ -18,11 +19,13 @@ def cart_add(request, product_id):
                  update_quantity=cd['update'])
     return redirect('cart:cart_detail')
 
+
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
+
 
 def cart_detail(request):
     cart = Cart(request)
@@ -30,6 +33,16 @@ def cart_detail(request):
             item['update_quantity_form'] = CartAddProductForm(
                               initial={'quantity': item['quantity'],
                               'update': True})
+
     coupon_apply_form = CouponApplyForm()
 
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products,
+                                                  max_results=4)
+
+    return render(request,
+                  'cart/detail.html',
+                  {'cart': cart,
+                   'coupon_apply_form': coupon_apply_form,
+                   'recommended_products': recommended_products})
